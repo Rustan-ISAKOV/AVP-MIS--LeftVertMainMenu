29c6714c70591a2936f7d8ea10b194f2ed23f433
Rustan-ISAKOV <45059671+Rustan-ISAKOV@users.noreply.github.com>
2019-01-14 22:38:02 +0600

    Create Left-Main-Menu-script.js

---
+++ Left-Main-Menu-script.js
@@ -0,0 +1,48 @@
+function menuInit() {
+	var dropdown = document.getElementById("dropdown-item");
+	var dropdown2 = document.getElementById("dropdown-item2");
+  var dropdown3 = document.getElementById("dropdown-item3");
+
+	var submenu = document.getElementById("submenu");
+	var submenu2 = document.getElementById("submenu2");
+  var submenu3 = document.getElementById("submenu3");
+
+
+	dropdown.onmouseover = popOutMenu;
+	dropdown.onmouseout = hideDropdown;
+	dropdown2.onmouseover = popOutMenu2;
+	dropdown2.onmouseout = hideDropdown2;
+	dropdown3.onmouseover = popOutMenu3;
+	dropdown3.onmouseout = hideDropdown3;  
+
+	submenu.onmouseover = popOutMenu;
+	submenu.onmouseout = hideDropdown;
+	submenu2.onmouseover = popOutMenu2;
+	submenu2.onmouseout = hideDropdown2;
+	submenu3.onmouseover = popOutMenu3;
+	submenu3.onmouseout = hideDropdown3;  
+}
+
+function popOutMenu () {
+	submenu.className = "show-submenu";
+}
+
+function hideDropdown () {
+	submenu.className = "hide-submenu";
+}
+
+function popOutMenu2 () {
+	submenu2.className = "show-submenu2";
+}
+
+function hideDropdown2 () {
+	submenu2.className = "hide-submenu2";
+}
+
+function popOutMenu3 () {
+	submenu3.className = "show-submenu3";
+}
+
+function hideDropdown3 () {
+	submenu3.className = "hide-submenu3";
+}
